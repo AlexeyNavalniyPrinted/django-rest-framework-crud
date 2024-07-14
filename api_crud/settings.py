4c1b9bb3a8d82bb5bca68cc0cc3748ab11b50886
AlexeyNavalniyPrinted <117517842+AlexeyNavalniyPrinted@users.noreply.github.com>
2024-07-14 14:47:48 +0300

    finish rollback.yml

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -35,7 +34,6 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
-
 
 # Application definition
 
@@ -84,21 +82,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_crud.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',# Change
-        'PORT': '5432'
+        'PASSWORD': open('/etc/secrets/password', 'r').read().strip(),
+        'HOST': 'db-postgresql.default.svc.cluster.local',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'sslcert': '/etc/secrets/certs/tls.crt',
+            'sslkey': '/etc/secrets/certs/tls.key',
+            'sslrootcert': '/etc/secrets/certs/ca.crt',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -118,7 +121,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -131,7 +133,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
